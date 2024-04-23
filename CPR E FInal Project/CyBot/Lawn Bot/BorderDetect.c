d7ac367ede5e458fdc0ba0b0e463b269611956ef
KadenBerger <125397505+KadenBerger@users.noreply.github.com>
2024-04-23 10:43:42 -0500

    Update BorderDetect.c

--- CPR E FInal Project/CyBot/Lawn Bot/BorderDetect.c
+++ CPR E FInal Project/CyBot/Lawn Bot/BorderDetect.c
@@ -15,4 +15,23 @@ uint32_t borderDetected(oi_t *sensor_data){
 
 }
 
+void moveForwardWithBorderDetect(oi_t *sensor_data, double distance_mm){
+    double sum = 0; // distance member in oi_t struct is type double
+    oi_setWheels(250,250); //move forward at full speed
+
+    while (sum < distance_mm) {
+        oi_update(sensor_data);
+        if(sensor_data -> cliffFrontLeftSignal > 2600 || sensor_data -> cliffFrontRightSignal > 2600){
+            oi_setWheels(0,0);
+            return;
+        }
+
+        sum += sensor_data -> distance; // use -> notation since pointer
+        sensor_data -> distance = 0;
+    }
+
+    oi_setWheels(0,0);
+    return;
+}
+
 
