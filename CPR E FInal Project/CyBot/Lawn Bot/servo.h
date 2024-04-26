bd868c397aaf387e1b8cc774dcf294fa4bc644df
Alexander Svobodny <118368311+Atlai-0@users.noreply.github.com>
2024-04-25 22:00:59 -0500

    Add files via upload

--- CPR E FInal Project/CyBot/Lawn Bot/servo.h
+++ CPR E FInal Project/CyBot/Lawn Bot/servo.h
@@ -5,9 +5,15 @@
 #include <stdbool.h>
 #include <inc/tm4c123gh6pm.h>
 #include "driverlib/interrupt.h"
+#include "button.h"
+
+unsigned long pwm_period = 0x4E200;
 
 void servo_init(void);
 
-void servo_move(uint16_t degrees);
+
+uint16_t servo_move(uint16_t degrees);
+
+void servo_calibrate();
 
 #endif /* SERVO_H_ */
